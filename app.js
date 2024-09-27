31def06b85cd981778333d1ba480da384a227eb0
Joseph <joseph8778@gmail.com>
2024-09-27 16:35:23 -0500

    sort low to high function with array objects added.

--- app.js
+++ app.js
@@ -1,46 +1,10 @@
-// function showRating(rate) {
-// let wholeNum = Math.floor(rate/1)
-// let decNum = rate % 1
-// let arr = []
-
-// arr.length = wholeNum
-// arr.fill("*")
-// if (decNum > 0) {
-//     arr.push('.')
-// }
-// return arr
-// }
-
-
-
-// console.log(showRating(4.5))
-
-
-
-// function showRating(rating) {
-//     let ratings = ""
-//     for (let i = 0; i < Math.floor(rating); ++i) {
-//         ratings = ratings + "*"
-//         if (i !== Math.floor(rating - 1)) {
-//             ratings +=  " "
-//         }
-//     }
-
-//     if (!Number.isInteger(rating)) {
-//         ratings = ratings + ' .'
-//     }
-//  return ratings
-// }
-
-// console.log(showRating(3.5))
-
-
-//BEST PRACTICE//
-
-function showRating(rating) {
-const stars = " *".repeat(Math.floor(rating))
-const fullStop = !Number.isInteger(rating) ? ' .' : ''
-return stars + fullStop;
+function sortLowTohigh(arr) {
+return arr.sort((a, b) => a.price - b.price
+)
 }
+console.log(sortLowTohigh([
+    { id: 1, price: 50 },
+    { id: 2, price: 0},
+    { id: 3, price: 500},
 
-console.log(showRating(4.5))
+]))
