68d0c9c252a485943c6f80e3aa45bf806e5689c4
Joseph <joseph8778@gmail.com>
2024-09-27 15:46:36 -0500

    best practice star rating function

--- app.js
+++ app.js
@@ -1,16 +1,46 @@
-function showRating(rate) {
-let wholeNum = Math.floor(rate/1)
-let decNum = rate % 1
-let arr = []
+// function showRating(rate) {
+// let wholeNum = Math.floor(rate/1)
+// let decNum = rate % 1
+// let arr = []
 
-arr.length = wholeNum
-arr.fill("*")
-if (decNum > 0) {
-    arr.push('.')
+// arr.length = wholeNum
+// arr.fill("*")
+// if (decNum > 0) {
+//     arr.push('.')
+// }
+// return arr
+// }
+
+
+
+// console.log(showRating(4.5))
+
+
+
+// function showRating(rating) {
+//     let ratings = ""
+//     for (let i = 0; i < Math.floor(rating); ++i) {
+//         ratings = ratings + "*"
+//         if (i !== Math.floor(rating - 1)) {
+//             ratings +=  " "
+//         }
+//     }
+
+//     if (!Number.isInteger(rating)) {
+//         ratings = ratings + ' .'
+//     }
+//  return ratings
+// }
+
+// console.log(showRating(3.5))
+
+
+//BEST PRACTICE//
+
+function showRating(rating) {
+const stars = " *".repeat(Math.floor(rating))
+const fullStop = !Number.isInteger(rating) ? ' .' : ''
+return stars + fullStop;
 }
-return arr
-}
-
-
 
 console.log(showRating(4.5))
